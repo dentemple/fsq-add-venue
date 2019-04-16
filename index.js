7db0fcc342f32cf0f92722c56966f8c040d12011
christineywang <wang.christiney@gmail.com>
2019-04-16 17:11:42 +0000

    Break out upload function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const args = require('yargs').argv;
+const yargs = require('yargs');
 const https = require('https');
 const fetch = require('node-fetch');
 const results = [];
@@ -10,85 +11,98 @@ const outputFile = args.output; // Output file of new venue IDs (or duplicate ID
 const token = args.token; // Your Foursquare OAuth token
 
 
-fs.createReadStream(inputFile)
-  .pipe(csv({ separator: '\t'} ))
-  .on('data', x => {
-    results.push({
-      name: x.name,
-      address: x.address,
-      city: x.city,
-      state: x.state,
-      zip: x.zip,
-      ll: x.ll,
-      primaryCategoryId: x.primaryCategoryId,
-      crossStreet: x.crossStreet,
-      phone: x.phone,
-      allCategoryIds: x.allCategoryIds,
-      parentId: x.parentId,
-      cc: x.cc,
-      twitter: x.twitter,
-      description: x.description,
-      url: x.url
+// Upload (default) command
+yargs.command(['upload', '*'], 'upload a file', (yargs) => {}, (argv) => {
+  console.log('Uploading your file now...');
+  upload();
+}).argv;
 
-});
-  })
-  .on('end', () => {
-    const venues = results;
-    const fetches = venues.map(venue => {
-      let urlParams = '';
-      const params = Object.keys(venue);
-      const values = Object.values(venue);
 
-      // Build the url parameters
-      for (param in params) {
-        urlParams += '&' + params[param] + '=' + encodeURIComponent(values[param])
-      }
+function upload() {
+  fs.createReadStream(inputFile)
+    .pipe(csv({
+      separator: '\t'
+    }))
+    .on('data', x => {
+      results.push({
+        name: x.name,
+        address: x.address,
+        city: x.city,
+        state: x.state,
+        zip: x.zip,
+        ll: x.ll,
+        primaryCategoryId: x.primaryCategoryId,
+        crossStreet: x.crossStreet,
+        phone: x.phone,
+        allCategoryIds: x.allCategoryIds,
+        parentId: x.parentId,
+        cc: x.cc,
+        twitter: x.twitter,
+        description: x.description,
+        url: x.url
 
-      const url = `https://api.foursquare.com/v2/venues/add?oauth_token=${token}&v=20190110${urlParams}`;
+      });
+    })
+    .on('end', () => {
+      const venues = results;
+      console.log(results);
+      const fetches = venues.map(venue => {
+        let urlParams = '';
+        const params = Object.keys(venue);
+        const values = Object.values(venue);
+        console.log(params, values);
+        // Build the url parameters
+        for (param in params) {
+          if (params[param] && values[param]) {
+            urlParams += '&' + params[param] + '=' + encodeURIComponent(values[param])
+          }
+        }
+
+        const url = `https://api.foursquare.com/v2/venues/add?oauth_token=${token}&v=20190110${urlParams}`;
 
         return fetch(url, {
-            method: 'post'
+          method: 'post'
         }).then(res => {
-            return res.json()
+          return res.json()
         }).then(data => {
-            const statusCode = data.meta.code;
-            if (statusCode === 409) {
-              const duplicateVenueId = data.response.candidateDuplicateVenues[0].id;
-              const duplicateVenueName = data.response.candidateDuplicateVenues[0].name;
-                return {
-                  duplicateVenueId,
-                  duplicateVenueName,
-                  statusCode
-                }
+          const statusCode = data.meta.code;
+          if (statusCode === 409) {
+            const duplicateVenueId = data.response.candidateDuplicateVenues[0].id;
+            const duplicateVenueName = data.response.candidateDuplicateVenues[0].name;
+            return {
+              duplicateVenueId,
+              duplicateVenueName,
+              statusCode
             }
-            if (statusCode === 200) {
-              const newVenueId = data.response.venue.id;
-              const newVenueName = data.response.venue.name;
-                return {
-                  newVenueId,
-                  newVenueName,
-                  statusCode
-                }
+          }
+          if (statusCode === 200) {
+            const newVenueId = data.response.venue.id;
+            const newVenueName = data.response.venue.name;
+            return {
+              newVenueId,
+              newVenueName,
+              statusCode
             }
-            if (statusCode === 400) {
-              const requestId = data.meta.requestId;
-              const errorMessage = data.meta.errorDetail;
-                return {
-                  requestId,
-                  errorMessage,
-                  statusCode
-                }
+          }
+          if (statusCode === 400) {
+            const requestId = data.meta.requestId;
+            const errorMessage = data.meta.errorDetail;
+            return {
+              requestId,
+              errorMessage,
+              statusCode
             }
+          }
         }).catch(err => {
-            console.log(err)
+          console.log(err)
         });
-    });
+      });
 
-    Promise.all(fetches).then((venues) => {
-      fs.writeFile(outputFile, JSON.stringify(venues), function(err){
-         if (err) console.log(err);
-         console.log('Upload complete and successfully written to file.');
+      Promise.all(fetches).then((venues) => {
+        fs.writeFile(outputFile, JSON.stringify(venues), function(err) {
+          if (err) console.log(err);
+          console.log('Upload complete and successfully written to file.');
         });
+      });
     });
-  });
-
+};
